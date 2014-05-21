1a43ca883e52d1f99c23879c91ddf24a224948d1
brendross <brendan.ross@cengage.com>
2014-05-21 15:05:46 -0400

    removed useless file

--- websockets.js
+++
@@ -1,9 +0,0 @@
-var socket = io.connect('http://localhost:3000');
-
-socket.on('connect', function() {
-	socket.emit('newPlayer');
-});
-
-socket.on('message', function(data) {
-	console.log(data);
-});
